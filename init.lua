5890d24ffa7e9dc50921afa72ff9d628a53fd51f
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2025-10-23 22:34:30 +0300

    fix: remove the dublicated settings

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
 -- Global Settings And Remaps
-require("settings")
-require("keybinds")
 require("autocommands")
 if vim.g.neovide then require("neovide") end
 
 vim.loader.enable()
 
-require("core.opts")
-
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 require("core.utils").bootstrap(lazy_path)
+require("keybinds")
+require("settings")
 
 return require("lazy").setup("plugins")
