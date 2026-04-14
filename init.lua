9d6dddb6ed71d78af30537760c78908683c739e6
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-04-14 23:45:55 +0200

    fix: disable zig autofmt

--- init.lua
+++ init.lua
@@ -842,6 +842,9 @@ local plugins = {
   {
     "https://codeberg.org/ziglang/zig.vim",
     ft = { "zig" },
+    init = function()
+      vim.g.zig_fmt_autosave = 0
+    end,
   },
 	
 	-- ========== Cosmetics & Utilities ==========
